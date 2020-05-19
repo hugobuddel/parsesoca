60353f5ea48e4bfec4cfb8ad57921b8941e79cbc
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-05-19 20:03:09 +0200

    Test many OCA files

---
+++ test/test_many.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Test classification."""
+import os
+from pathlib import Path
+from pprint import pprint
+
+from parsesoca.ocalexer import OCALexer
+from parsesoca.ocaparser import OCAParser
+
+
+def do_test_oca_file(fn_oca):
+    """Parse an OCA file."""
+
+    soca = open(fn_oca).read()
+
+    soca_clean = "\n".join(
+        (line + "//")[:(line + "//").index("//")] for line in soca.splitlines()
+    )
+
+    mylexer = OCALexer()
+    for token in mylexer.tokenize(soca_clean):
+        print(token)
+
+    myparser = OCAParser()
+    pprint(myparser.parse(mylexer.tokenize(soca_clean)))
+    if hasattr(myparser, 'errorok'):
+        assert myparser.errorok, "Error"
+
+
+def test_ocas():
+    """Test many OCA files."""
+    path_here = Path(__file__).resolve().parent
+    for dirpath, _, filenames in os.walk(path_here):
+        fns_oca = [fn for fn in filenames if fn.endswith(".oca")]
+        fns_oca.sort(key=lambda fn: fn != 'foundproblems.oca')
+        for fn_oca in fns_oca:
+            print()
+            print(fn_oca)
+            do_test_oca_file(os.path.join(dirpath, fn_oca))
+
