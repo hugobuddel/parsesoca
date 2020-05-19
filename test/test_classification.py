0b136b536f616be2283b284b8704c465bebbfff0
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-05-19 15:02:38 +0200

    Allow comments

--- test/test_classification.py
+++ test/test_classification.py
@@ -9,6 +9,8 @@ def test_classification():
     """Test classification."""
 
     soca = """
+    //CLASSIFICATION
+
     if DPR.CATG == "CALIB" and DPR.TECH == "IMAGE" and DPR.TYPE == "DARK"
       and INSTRUME like "%HAWKI%"
       and TPL.NEXP > 2 then
@@ -25,9 +27,14 @@ def test_classification():
     }
     """
 
+    soca_clean = "\n".join(
+        line for line in soca.splitlines()
+        if not (line.strip() + "//").startswith("//")
+    )
+
     mylexer = OCALexer()
-    for token in mylexer.tokenize(soca):
+    for token in mylexer.tokenize(soca_clean):
         print(token)
 
     myparser = OCAParser()
-    print(myparser.parse(mylexer.tokenize(soca)))
+    print(myparser.parse(mylexer.tokenize(soca_clean)))
