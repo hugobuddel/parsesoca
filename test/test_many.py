7f76deda6a7c4f872e6fe17e998361a38ca29005
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-05-20 14:53:27 +0200

    Support multiline comments

--- test/test_many.py
+++ test/test_many.py
@@ -8,13 +8,27 @@ from parsesoca.ocalexer import OCALexer
 from parsesoca.ocaparser import OCAParser
 
 
+def cleanlines(lines):
+    """Remove comments."""
+    # TODO: refactor this in a general function
+    in_comment = False
+    for line in lines:
+        if in_comment:
+            if line.strip() == "*/":
+                in_comment = False
+        elif line.strip() == "/*":
+            in_comment = True
+        else:
+            yield (line + "//")[:(line + "//").index("//")]
+
+
 def do_test_oca_file(fn_oca):
     """Parse an OCA file."""
     print("do_test_oca_file", fn_oca)
     soca = open(fn_oca).read()
 
     soca_clean = "\n".join(
-        (line + "//")[:(line + "//").index("//")] for line in soca.splitlines()
+         line for line in cleanlines(soca.splitlines())
     )
 
     mylexer = OCALexer()
